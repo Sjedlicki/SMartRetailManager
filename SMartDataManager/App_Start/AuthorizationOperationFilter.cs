18989eada12d42aa0001aa20a30b11cdaf526df0
steve <jedi1014@comcast.net>
2020-09-12 01:22:39 -0400

    Add Swagger documentation to API
    Adds Swashbuckler/Swagger to the API for documentation and testing

---
+++ SMartDataManager/App_Start/AuthorizationOperationFilter.cs
@@ -0,0 +1,30 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+namespace SMartDataManager.App_Start
+{
+    public class AuthorizationOperationFilter : IOperationFilter
+    {
+        // Adding a parameter to every operation
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+            if(operation.parameters == null)
+            {
+                operation.parameters = new List<Parameter>();
+            }
+
+            operation.parameters.Add(new Parameter
+            {
+                name = "Authorization",
+                @in = "header",
+                description = "access token", // documentaion for it
+                required = false,
+                type = "string"
+            });
+        }
+    }
+}
