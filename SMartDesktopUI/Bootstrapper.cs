2b23f7ecf977dd9d4fd2c5614120ee84561bbde4
steve <jedi1014@comcast.net>
2020-09-12 02:02:26 -0400

    Added WPF App
    Configured ShellView for WPF

---
+++ SMartDesktopUI/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using Caliburn.Micro;
+using SMartDesktopUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SMartDesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            // On Startup Launch ShellViewModel as our Base View
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
