91b247b7eae7a2f4490e8d999ccb0f2ab6b980a7
steve <jedi1014@comcast.net>
2020-09-12 02:36:42 -0400

    Adds MVVM
    Configures with Caliburn Micro for our MVVM framework.
    Also, includes the Simple Container for DI system.

--- SMartDesktopUI/Bootstrapper.cs
+++ SMartDesktopUI/Bootstrapper.cs
@@ -3,23 +3,59 @@ using SMartDesktopUI.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace SMartDesktopUI
 {
+    // Adding a dependency injection system.
     public class Bootstrapper : BootstrapperBase
     {
+        // Dependecy Injection is going to handle the instantiation of our classes.
+        private SimpleContainer _container = new SimpleContainer();
+
         public Bootstrapper()
         {
             Initialize();
+        }
+
+        protected override void Configure()
+        {
+            // The container holds an instance of itself to pass out when asked for a simple container.
+            _container.Instance(_container);
+
+            // Singleton is One instance of the class for the life of the application.
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
         }
 
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             // On Startup Launch ShellViewModel as our Base View
             DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            // Get's the instance from a container.
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
         }
     }
 }
